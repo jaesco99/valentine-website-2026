e4180b3962db877eff5e15ca2d45f9b558f5c8e6
Javier Francisco Escalante Cordero <JAESCO92@GMAIL.COM>
2026-02-01 14:30:28 +0100

    Update music streaming URL in config.js

--- config.js
+++ config.js
@@ -79,7 +79,7 @@ const CONFIG = {
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: "https://res.cloudinary.com/dncywqfpb/video/upload/v1738399057/music_qrhjvy.mp3", // Music streaming URL
+        musicUrl: "https://res.cloudinary.com/dtms74ga0/video/upload/v1769952508/A_AP_ROCKY_-_STAY_HERE_VISUALIZER_a11bv3.mp3", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
         volume: 0.5                        // Volume level (0.0 to 1.0)
